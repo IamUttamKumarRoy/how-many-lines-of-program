b170ea7650c3a8d0aaff1475068a591a5d742612
Uttam Kumar Roy <cseuttamiu@gmail.com>
2017-03-24 20:40:41 +0600

    Program Files

---
+++ ProgramLineCounter/FileManagement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramLineCounter
+{
+    class FileManagement
+    {
+        public string[] getFileList( string directoryname,string file_type )
+        {
+            List<string> list = new List<string>();
+            try
+            {
+                if (Directory.Exists(@"" + directoryname))
+                {
+                    DirectoryInfo d = new DirectoryInfo(@"" + directoryname);
+                    FileInfo[] Files = d.GetFiles( "*."+ file_type );
+
+                    foreach (FileInfo file in Files)
+                    {
+                        list.Add(file.Name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return list.ToArray();
+        }
+        public int getLineCountOfFile( string file_path )
+        {
+            int lineCount = 0;
+            string temp ="";
+            using ( var reader = File.OpenText( @"" + file_path ) )
+            {
+                while (reader.ReadLine() != null)
+                {
+                    temp = reader.ReadLine();
+                    if ( !string.IsNullOrEmpty(temp) )
+                    {
+                        lineCount++;
+                    }
+                }
+            }
+            return lineCount;
+        }
+    }
+}
---
+++ ProgramLineCounter/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgramLineCounter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void line_count_btn_Click(object sender, RoutedEventArgs e)
+        {                        
+            string directory_address = directory_address_tbox.Text;
+            ComboBoxItem fileTypeCB = (ComboBoxItem)file_type_cbox.SelectedItem;
+            string file_type = fileTypeCB.Name.ToString();
+            
+            bool is_input_ok = true;
+            if ( String.IsNullOrEmpty(file_type) )
+            {
+                is_input_ok = false;
+                file_type_cbox.Focus();
+            }
+            if (String.IsNullOrEmpty(directory_address))
+            {
+                is_input_ok = false;
+                directory_address_tbox.Focus();
+            }
+            int line_count = 0;
+            int total_line = 0;
+            if (is_input_ok )
+            {
+                FileManagement fmClassObj = new FileManagement();
+                string[] files_list = fmClassObj.getFileList(directory_address, file_type);
+                string file_line_details = "";
+                foreach (string file in files_list)
+                {
+                    line_count = fmClassObj.getLineCountOfFile(@""+directory_address+"\\"+ file);
+                    file_line_details = file_line_details + file + " = " + line_count  + "\n";
+                    total_line += line_count;
+                }
+                line_count_number_label.Content = total_line;
+                file_list_rtbox.Document.Blocks.Clear();
+                file_list_rtbox.AppendText(file_line_details);
+            }
+        }
+
+        private void about_btn_Click(object sender, RoutedEventArgs e)
+        {
+            About about = new About();
+            about.ShowDialog();
+        }
+    }
+}
